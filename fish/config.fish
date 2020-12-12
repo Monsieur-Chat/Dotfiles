26a9126aee891d8353e1e59a96e4ef469310c74f
0xE <49166384+0x307845@users.noreply.github.com>
2020-12-12 22:46:35 +0000

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,5 @@ set -g theme_color_host                            cyan
 set -g theme_color_user                            purple
 set -g theme_display_rw no
 set -g theme_color_superuser                       red
-set -g theme_prompt_char_normal                    '➜'
+set -g theme_prompt_char_normal                    '❯'
 set -g theme_prompt_userhost_separator             ':'
