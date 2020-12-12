466ab4bfce6710887c6d318de374132baa113376
0xE <49166384+0x307845@users.noreply.github.com>
2020-12-12 19:12:57 +0000

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+#Set Color and Prompt option
+set -g theme_display_time yes
+set -g theme_display_group no
+set -g theme_color_host                            cyan
+set -g theme_color_user                            purple
+set -g theme_display_rw no
+set -g theme_color_superuser                       red
+set -g theme_prompt_char_normal                    '➜'
+set -g theme_prompt_userhost_separator             ':'
